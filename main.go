5318e644f9dbf0c01ad69f9b6ad6236253c9ccc5
wlfp <68606149+wlfp@users.noreply.github.com>
2025-11-18 15:10:45 +0000

    feat: add a better entry flow for the algorithm

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/wlfp/bioinfo_algos/alignment"
+import (
+	"fmt"
+
+	"github.com/wlfp/bioinfo_algos/alignment"
+)
 
 func main() {
-	alignment.AlignmentExample()
+
+	algorithmChoices := [...]string{"Global alignment"}
+
+	var algorithmChoice int
+ChooseAlgorithm:
+	fmt.Println("Please select an algorithm, the following choices are available:")
+	for index, algorithmName := range algorithmChoices {
+		fmt.Printf("%d. %s\n", index, algorithmName)
+	}
+	fmt.Scanln(&algorithmChoice)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+
+	switch algorithmChoice {
+	case 0:
+		alignment.Alignment()
+	default:
+		fmt.Println("That algorithm wasn't recognised, try again?")
+		goto ChooseAlgorithm
+	}
 }
